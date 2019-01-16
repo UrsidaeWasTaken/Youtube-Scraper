0bc42793b8448015ccbef1b23f492d015144aaf9
UrsidaeWasTaken <ursidaemail@gmail.com>
2019-01-16 08:29:13 +0000

    Updated Youtube Scrape • Added Extra configs in scrape.py
    Exceptions, Configs and more video info categories on scrape.py

--- scrape.py
+++ scrape.py
@@ -9,9 +9,12 @@ config = configparser.ConfigParser()
 config.read('config.ini')
 
 API_KEY = config['client']['api_key']
+VIDEO_INFO_URL = config['base_url']['video_info']
+SEARCH_QUERY_URL = config['base_url']['search_query']
 
-def video_info(video_id):
-    url = requests.get("https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=%s&key=%s" % (video_id, API_KEY))
+# Stores video information in csv
+def video_info(video_id, keyword=None):
+    url = requests.get(VIDEO_INFO_URL .format(id=video_id, key=API_KEY))
     sleep(1)
     data = url.json()
 
@@ -20,34 +23,56 @@ def video_info(video_id):
     video_published = video_info['snippet']['publishedAt'][:9]
     video_channel, video_channel_id = video_info['snippet']['channelTitle'], video_info['snippet']['channelId']
     video_title = video_info['snippet']['title']
-    video_views = video_info['statistics']['viewCount']
-    video_likes, video_dislikes = int(video_info['statistics']['likeCount']), int(video_info['statistics']['dislikeCount'])
-    video_rating = (video_likes/(video_likes+video_dislikes))*100
-    video_comments = video_info['statistics']['commentCount']
+    try:
+        video_views = video_info['statistics']['viewCount']
+        views_disabled = False
+    except KeyError:
+        video_views = "0"
+        views_disabled = True
+    try:
+        video_likes, video_dislikes = int(video_info['statistics']['likeCount']), int(video_info['statistics']['dislikeCount'])
+        video_rating = (video_likes/(video_likes+video_dislikes))*100
+        ratings_disabled = False
+    except KeyError:
+        video_likes = video_dislikes = 0
+        video_rating = 0
+        ratings_disabled = True
+    try:
+        video_comments = video_info['statistics']['commentCount']
+        comments_disabled = False
+    except KeyError:
+        video_comments = "0"
+        comments_disabled = True
     is_live = video_info['snippet']['liveBroadcastContent']
-    video_data = [video_id, video_published, video_title, video_channel_id, video_channel, video_views, video_rating, video_likes, video_dislikes, video_comments, is_live]
 
-    with open('videos_data.csv', 'a') as csv_file:
+    video_data = [video_id, video_published, video_title, video_channel_id, video_channel,
+    video_views, video_rating, video_likes, video_dislikes, video_comments, views_disabled,
+    ratings_disabled, comments_disabled, is_live, keyword]
+
+    with open('videos_data.csv', 'a', encoding="utf-8") as csv_file:
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(video_data)
 
     return
 
-# Collect 
-def collect_keyword_search(keyword):
+# Collect top 20 videos with specific keyword in the title
+def collect_keyword_search():
+    keyword = input("Enter keyword to search... ").lower()
+    keyword = keyword.replace("#", "%23")
     keyword = keyword.replace("+", "%2B")
     keyword = keyword.replace(" ", "+")
-    url = requests.get("https://www.youtube.com/results?search_query=allintitle:%s" % keyword)
+    url = requests.get(SEARCH_QUERY_URL .format(keyword))
+    print("Fetching videos...\n")
     sleep(1)
     src = url.content
     soup = BeautifulSoup(src, 'lxml')
     videos = soup.findAll(class_='yt-uix-tile-link')
-    print(len(videos))
+    print("Adding videos to csv...\n")
     for video in videos:
         if "/watch?v=" in video['href']:
             video_id = video['href'][9:]
-            video_info(video_id)
+            video_info(video_id, keyword)
     return
 
-collect_keyword_search("Youtube")
+collect_keyword_search()
 print("CSV has been updated...")
